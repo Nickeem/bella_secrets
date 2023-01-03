10203da685d036c7293bdcd69a794e6d5b82d2df
Nickeem <vacation1478@gmail.com>
2023-01-03 19:58:04 -0400

    Landing Page decent state

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<slot></slot>
+
+<script>
+    import bg_image from '$lib/images/blurry-gradient-haikei.svg';
+</script>
+<style>
+    :root {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+        --bella-primary: #4D0B62;
+        --bella-secondary: #FDBE72;
+        background-image: url('$lib/images/triangle-background.webp');
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+</style>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Bella Secrets Home Page" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Allura&display=swap">
+</svelte:head>
+
+<section>
+    <h1>Bella Secrets</h1>
+    <a href="/inventory">View inventory</a>
+</section>
+
+<style>
+
+    a {
+        text-decoration: none;
+        color: #000;
+        padding: 14px;
+        border: black solid 1px;
+        background-color: white;
+    }
+
+    a:hover {
+        background-color: var(--bella-secondary);
+
+    }
+    
+    section {
+        margin-top: 20%;
+        text-align: center;
+    }
+    
+    h1 {
+        color: var(--bella-secondary);
+        font-family: 'Allura', cursive;
+        font-size: 80px;
+        text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
+    }
+</style>
